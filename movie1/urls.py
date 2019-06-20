7177dc6dd2cce4ee5d45e92b9104c43699082d29
Shiv Sharma <shivsharmavictory@gmail.com>
2019-06-20 21:14:46 +0530

    some changes

---
+++ movie1/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.index,name='index'),
+    #url('app',views.introduction,name=''),
+    #url('db',views.index1,name=''),
+    #Authentication creativity
+    #url('login/',views.user_login,name="user_login"),
+    #url('success/',views.success,name="user_success"),
+    #url('logout/',views.user_logout,name="user_logout"),
+
+]
--- shiv1/urls.py
+++ shiv1/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
 
-assert isinstance(path,object )
-from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movie1/', include())
-    
-    
+    url('movie1/',include('movie1.urls')),
 ]
