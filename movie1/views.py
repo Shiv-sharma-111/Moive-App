1bf7708d0d796985251411804e1c0970195e0089
Shiv Sharma <shivsharmavictory@gmail.com>
2019-06-20 23:38:42 +0530

    add songs name

--- movie1/views.py
+++ movie1/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from . models import Movie
 from django.http import HttpResponse
+from django.http import Http404
+from django .template import loader
 
 def index(request):
-    return HttpResponse("<h2>hii this is shivam sharma</h2>")
+    all_movies= Movie.objects.all()
+    template = loader.get_template('movie1/index.html')
+    context = {
+      'all_movies':all_movies,
+    }
+    #for a in all_movies:
+        #url = '/movie1/'+ str(a.id) + '/'
+        #html = '<a href = " '+ url + ' ">' + a.actor + '</a><br>'
+    return HttpResponse(template.render(context, request))
 
 def detail(request, movie_id):
-    return HttpResponse("<h2>welcome in id :" + str(movie_id) + "</h2>")
+    #return HttpResponse("<h2>welcome in id :" + str(movie_id) + "</h2>")
+    try:
+        m1 = Movie.objects.get(pk=movie_id)
+    except Movie.DoesNotExist
+        raise Http404("this is wrong")
+
+    return HttpResponse(template.render(context, request))
